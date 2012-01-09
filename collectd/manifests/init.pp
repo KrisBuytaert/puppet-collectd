c4632adaf838f082aee89eb230adfb1acd87c50b
kbuytaert <kbuytaert@8b6e57f2-9a8d-40ec-8078-61590d3dfd47>
2012-01-09 12:05:09 +0000

    Initial commit of module collectd
    git-svn-id: http://svn.dev.unifiedpost.com/repos/it/puppet/modules/collectd/trunk@3 8b6e57f2-9a8d-40ec-8078-61590d3dfd47

---
+++ collectd/manifests/init.pp
@@ -0,0 +1,74 @@
+# Class: collectd
+#
+# This module manages collectd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+
+class collectd::graphitewriter ( $graphitehost, $graphiteport) { 
+
+  notify {"Value Host is ${graphitehost}.":}
+  notify {"Value Port is ${graphiteport}.":}
+
+  file { '/usr/local/collectd-plugins/':
+    ensure => 'directory', 
+    group  => '0',
+    mode   => '755',
+    owner  => '0',
+}
+  file { '/usr/local/collectd-plugins/carbon_writer.py': 
+    ensure => 'file',
+    group  => '0',
+    mode   => '644',
+    owner  => '0',
+    source => 'puppet:///collectd/collectd-carbon/carbon_writer.py'
+}
+
+
+
+  file {
+     "/etc/collectd.d/graphite-writer.conf":
+       group   => '0',
+       mode    => '644',
+       owner   => '0',
+       require => Package['collectd'],
+       notify  => Service['collectd'], 
+       content => template("collectd/graphite-writer.conf.erb");
+   }
+
+}
+
+
+
+class collectd  { 
+
+
+  package {"collectd":
+    ensure => present;
+  }
+
+  # Required with a patch to include they Python LDLIB path as documented 
+  # on  https://github.com/indygreg/collectd-carbon
+  file {
+    "/etc/init.d/collectd":
+      group  => '0',
+      mode   => '755',
+      owner  => '0',
+      source => "puppet:///collectd/collectd";
+  }
+
+
+
+
+  service {"collectd":
+    ensure  => running,
+    require => [File['/etc/init.d/collectd'],Package['collectd']];
+  }
+}
