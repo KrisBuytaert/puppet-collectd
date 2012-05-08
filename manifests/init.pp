2198548052d3e8d477634309e3ecfe9ba0340283
Kris Buytaert <sdog@inuits.be>
2012-05-08 11:21:35 +0200

    Refactored collectd module to split off plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,39 +12,6 @@
 #
 # [Remember: No empty lines between comments and class definition]
 
-class collectd::graphitewriter ( $graphitehost, $graphiteport) {
-
-
-  file { '/usr/local/collectd-plugins/':
-    ensure => 'directory',
-    group  => '0',
-    mode   => '0755',
-    owner  => '0',
-  }
-  file { '/usr/local/collectd-plugins/carbon_writer.py':
-    ensure => 'file',
-    group  => '0',
-    mode   => '0644',
-    owner  => '0',
-    source => 'puppet:///modules/collectd/collectd-carbon/carbon_writer.py'
-  }
-
-
-
-  file {
-    '/etc/collectd.d/graphite-writer.conf':
-      group   => '0',
-      mode    => '0644',
-      owner   => '0',
-      require => Package['collectd'],
-      notify  => Service['collectd'],
-      content => template('collectd/graphite-writer.conf.erb');
-  }
-
-}
-
-
-
 class collectd  {
 
 
--- manifests/plugin/snmp.pp
+++ manifests/plugin/snmp.pp
@@ -1,4 +1,4 @@
-class collectd::snmp{
+class collectd::plugin::snmp{
 
 
   package {
