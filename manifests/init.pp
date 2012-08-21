3413b73d4ce432b2527d5b683f5b551dfcd573d4
Jan Vansteenkiste <jan@vstone.eu>
2012-08-21 21:53:53 +0200

    Move to autoload layout

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,44 +11,7 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-
-class collectd::graphitewriter ( $graphitehost, $graphiteport) {
-
-  notify {"Value Host is ${graphitehost}.":}
-  notify {"Value Port is ${graphiteport}.":}
-
-  file { '/usr/local/collectd-plugins/':
-    ensure => 'directory',
-    group  => '0',
-    mode   => '755',
-    owner  => '0',
-}
-  file { '/usr/local/collectd-plugins/carbon_writer.py':
-    ensure => 'file',
-    group  => '0',
-    mode   => '644',
-    owner  => '0',
-    source => 'puppet:///collectd/collectd-carbon/carbon_writer.py'
-}
-
-
-
-  file {
-     "/etc/collectd.d/graphite-writer.conf":
-       group   => '0',
-       mode    => '644',
-       owner   => '0',
-       require => Package['collectd'],
-       notify  => Service['collectd'],
-       content => template("collectd/graphite-writer.conf.erb");
-   }
-
-}
-
-
-
 class collectd  {
-
 
   package {"collectd":
     ensure => present,
