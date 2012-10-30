7b63ce744a7295e67ea29304cf89c99829eb3861
Kris Buytaert <sdog@inuits.be>
2012-10-30 13:26:48 +0100

    Adding apache template

---
+++ manifests/plugin/apache.pp
@@ -0,0 +1,17 @@
+class collectd::plugin::apache{
+
+  # On our debian boxen the http://localhost/server-status?auto is enabled
+  # Needs additional changes to the http config if on other distro therefore
+  # not including it by default
+
+  if ($::operatingsystem == 'Debian' or $::operatingsystem == 'Ubuntu')
+  {
+    file { '/etc/collectd.d/apache.conf':
+      content => template('collectd/plugin/apache.conf'),
+      group   => '0',
+      mode    => '0644',
+      owner   => '0',
+      notify  => Service['collectd'],
+    }
+  }
+}
