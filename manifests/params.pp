77f8611c1f15ccbe068dca781e24a8882068c9fd
Kris Buytaert <sdog@inuits.be>
2014-03-04 17:48:35 +0100

    Adding template that includes Include   for centos too due to broken package

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,15 @@ class collectd::params (
     /(?i:centos|redhat|fedora)/ => "collectd.$::architecture",
     default                     => 'collectd',
   },
-  $config_file    = '/etc/collectd/collectd.conf',
+  $config_file    = $::operatingsystem ? {
+    /(?i:centos|redhat|fedora)/ => '/etc/collectd.conf',
+    default                     => '/etc/collectd/collectd.conf',
+  },
+  $config_template_name = $::operatingsystem ? {
+    /(?i:centos|redhat|fedora)/ => 'collectd/collectd.conf.CentOS',
+    /(?i:Debian|Ubuntu)/        => 'collectd/collectd.conf.Debian',
+    default                     => 'collectd/collectd.conf.CentOS',
+  },
   $config_dir     = '/etc/collectd.d',
   $purge          = true,
   $service_name   = 'collectd',
