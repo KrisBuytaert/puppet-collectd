ff59e9bd5c369942bd6263a17bf8ea7df16f3703
Kris Buytaert <sdog@inuits.be>
2014-03-04 17:48:51 +0100

    Support for write_riemann

---
+++ manifests/plugin/write_riemann.pp
@@ -0,0 +1,19 @@
+class collectd::plugin::write_riemann($riemannhost = '127.0.0.1')
+{
+
+  package { 'collectd-write_riemann':
+    ensure => 'present',
+  }
+
+  file {
+    '/etc/collectd.d/write_riemann.conf':
+      group   => '0',
+      mode    => '0644',
+      owner   => '0',
+      require => Package['collectd'],
+      notify  => Service['collectd'],
+      content => template('collectd/write_riemann.conf.erb');
+  }
+
+}
+
