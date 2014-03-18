a030ffa6873e8726231ba00e5f0e77779fdbc4b1
Yvan Cottyn <vanten@gmail.com>
2014-03-18 11:18:39 +0100

    Added plugin for RabbitMQ.

---
+++ manifests/plugin/rabbitmq.pp
@@ -0,0 +1,40 @@
+class collectd::plugin::rabbitmq{
+
+  $collectd_rabbitmq_user  = hiera('collectd_rabbitmq_user')
+  $collectd_rabbitmq_password  = hiera('collectd_rabbitmq_password')
+
+  package { 'perl-Collectd':
+    ensure => 'present',
+  }
+
+  file { '/usr/local/collectd-plugins/Collectd/Plugins/RabbitMQ.pm':
+    source => 'puppet:///modules/collectd/RabbitMQ.pm',
+    mode   => '0755',
+  }
+
+  file_line { 'cpusummaryline':
+    path  => '/usr/share/collectd/types.db',
+    line  => 'cpusummary              user:COUNTER:U:U, nice:COUNTER:U:U, system:COUNTER:U:U, idle:COUNTER:U:U, iowait:COUNTER:U:U, irq:COUNTER:U:U, softirq:COUNTER:U:U, cpucount:GAUGE:U:U',
+    match => '^cpusummary\s+',
+    require => Package['collectd'],
+    notify  => Service['collectd'],
+  }
+
+  file_line { 'rabbitmqline':
+    path  => '/usr/share/collectd/types.db',
+    line  => 'rabbitmq                messages:GAUGE:0:U, messages_rate:GAUGE:0:U, messages_unacknolwedged:GAUGE:0:U, messages_unacknowledged_rate:GAUGE:0:U, messages_ready:GAUGE:0:U, message_ready_rate:GAUGE:0:U, memory:GAUGE:0:U, consumers:GAUGE:0:U, publish:GAUGE:0:U, publish_rate:GAUGE:0:U, deliver_no_ack:GAUGE:0:U, deliver_no_ack_rate:GAUGE:0:U, deliver_get:GAUGE:0:U, deliver_get_rate:GAUGE:0:U',
+    match => '^rabbitmq\s+',
+    require => Package['collectd'],
+    notify  => Service['collectd'],
+  }
+
+  file { '/etc/collectd.d/RabbitMQ.conf':
+    content => template('collectd/RabbitMQ.conf.erb'),
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    require => Package['collectd'],
+    notify  => Service['collectd'],
+  }
+
+}
