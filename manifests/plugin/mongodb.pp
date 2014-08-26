681de15ca5eac16407a210e7074b635d2b30cb15
kayn <pulecp@gmail.com>
2014-08-26 14:12:57 +0200

    added monitoring of mongodb over python module

---
+++ manifests/plugin/mongodb.pp
@@ -0,0 +1,30 @@
+class collectd::plugin::mongodb
+{
+
+  $mongod_bind_ip = hiera('mongod_bind_ip')
+
+  package {'pymongo':
+    ensure => 'present',
+    provider => 'pip',
+  }
+
+  file { '/usr/local/collectd-plugins/mongodb.py':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
+    content => template('collectd/mongo.py.erb'),
+  }
+
+  file { '/etc/collectd.d/mongodb.conf':
+    ensure => 'file',
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    content => template('collectd/mongo.conf.erb'),
+    require => Package['pymongo']
+    require => File['/usr/local/collectd-plugins/mongodb.py']
+    notify  => Service['collectd'],
+  }
+
+}
