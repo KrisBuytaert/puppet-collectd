5c4e548b6dafbc7d82f8c63f3f50243addc1b15e
kayn <pulecp@gmail.com>
2014-08-26 09:55:22 +0200

    initialization postfix plugin to collectd

---
+++ manifests/plugin/postfix.pp
@@ -0,0 +1,30 @@
+# Class: collectd
+#
+# This module manages collectd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+
+class collectd::plugin::postfix () {
+
+  file {
+    '/etc/collectd.d/postfix.conf':
+      group   => '0',
+      mode    => '0644',
+      owner   => '0',
+      require => Package['collectd'],
+      notify  => Service['collectd'],
+      source => 'puppet:///modules/collectd/plugin/postfix.conf'
+  }
+
+}
+
+
+
