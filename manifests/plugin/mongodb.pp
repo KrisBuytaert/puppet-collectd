5cfa25020196bd225f5d00e8ba6489395f5c5cdc
kayn <pulecp@gmail.com>
2014-08-26 15:05:03 +0200

    added missing python-pip package

--- manifests/plugin/mongodb.pp
+++ manifests/plugin/mongodb.pp
@@ -3,9 +3,16 @@ class collectd::plugin::mongodb
 
   $mongod_bind_ip = hiera('mongod_bind_ip')
 
+  if ! defined(Package['python-pip']) {
+    package { 'python-pip':
+      ensure => present,
+    }
+  }
+
   package {'pymongo':
     ensure => 'present',
     provider => 'pip',
+    require => Package['python-pip'],
   }
 
   file { '/usr/local/collectd-plugins/mongodb.py':
