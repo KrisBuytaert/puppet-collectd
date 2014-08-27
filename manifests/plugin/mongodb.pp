edf37ced6f402e7e2a38e874cce6d36a824ef59c
kayn <pulecp@gmail.com>
2014-08-27 15:32:23 +0200

    fix mail_counter/types.db error

--- manifests/plugin/mongodb.pp
+++ manifests/plugin/mongodb.pp
@@ -31,8 +31,15 @@ class collectd::plugin::mongodb
     mode    => '0644',
     owner   => '0',
     content => template('collectd/mongodb.conf.erb'),
-    require => [ Package['pymongo'], File['/usr/local/collectd-plugins/mongodb.py'] ],
+    require => [ Package['pymongo'], File['/usr/local/collectd-plugins/mongodb.py'], File_line['types.db'] ],
     notify  => Service['collectd'],
   }
+
+  file_line { 'types.db':
+    ensure => present,
+    line   => 'mail_counter            value:COUNTER:0:65535',
+    path   => '/usr/share/collectd/types.db',
+  }
+
 
 }
