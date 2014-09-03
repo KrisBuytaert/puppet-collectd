5344ed82aab933ecaa28dbe69369a3070468ccca
kayn <pulecp@gmail.com>
2014-09-03 11:02:42 +0200

    mongo - added notify to file_line to restart collectd

--- manifests/plugin/postfix.pp
+++ manifests/plugin/postfix.pp
@@ -15,19 +15,21 @@
 class collectd::plugin::postfix {
 
   file { '/etc/collectd.d/postfix.conf':
-      group   => '0',
-      mode    => '0644',
-      owner   => '0',
-      require => [ Package['collectd'], File_line['postfixline'] ],
-      notify  => Service['collectd'],
-      source => 'puppet:///modules/collectd/plugin/postfix.conf',
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    require => [ Package['collectd'], File_line['postfixline'] ],
+    notify  => Service['collectd'],
+    source => 'puppet:///modules/collectd/plugin/postfix.conf',
   }
 
   file_line { 'postfixline':
     ensure => present,
-    line   => 'mail_counter            value:COUNTER:0:65535 ',
+    line   => 'mail_counter            value:COUNTER:0:65535',
     match  => '^mail_counter\s+',
     path   => '/usr/share/collectd/types.db',
+    require => Package['collectd'],
+    notify  => Service['collectd'],
   }
 
 }
