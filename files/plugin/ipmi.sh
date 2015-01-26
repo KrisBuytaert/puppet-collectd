241a942f7d5b71d4a1d4b6c2b5080f1bbb43f193
honza <honza@inuits.eu>
2015-01-26 12:09:31 +0000

    added ipmi plugin to collectd
    Signed-off-by: honza <honza@inuits.eu>

---
+++ files/plugin/ipmi.sh
@@ -0,0 +1,1 @@
+sudo /usr/local/collectd-plugins/./ipmi-collectd
---
+++ manifests/plugin/ipmi.pp
@@ -0,0 +1,43 @@
+class collectd::plugin::ipmi {
+
+  user { 'collectd':
+    ensure => present,
+    name   => 'collectd',
+    shell  => '/sbin/nologin',
+    system => true,
+  }
+
+  file { '/usr/local/collectd-plugins/ipmi-collectd':
+    ensure => 'file',
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
+    source => 'puppet:///modules/collectd/plugin/ipmi-collectd',
+    notify => Service['collectd'],
+  }
+
+  file { '/usr/local/collectd-plugins/ipmi.sh':
+    ensure => 'file',
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
+    source => 'puppet:///modules/collectd/plugin/ipmi.sh',
+    notify => Service['collectd'],
+  }
+
+  file { '/etc/collectd.d/ipmi.conf':
+    ensure => 'file',
+    group  => 'root',
+    mode   => '0644',
+    owner  => 'root',
+    source => 'puppet:///modules/collectd/plugin/ipmi.conf',
+    notify => Service['collectd'],
+  }
+
+  sudo::conf{'ipmi_collectd':
+    content => "Defaults:collectd !requiretty
+    nagios ALL=(ALL) /usr/bin/collectd-ipmitool\n",
+  }
+
+
+}
