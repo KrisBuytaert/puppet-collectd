fa7132478103ff489f1296e74d499a7102c5cde7
Pavel Pulec <kayn@inuits.eu>
2015-03-26 16:04:29 +0100

    refs #14015, redis - renamed couple of stats and added miss/hit ratio
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- files/plugin/redis-stat.sh
+++ files/plugin/redis-stat.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
- 
+
 HOSTNAME="${COLLECTD_HOSTNAME:-`hostname -f`}"
 INTERVAL="${COLLECTD_INTERVAL:-10}"
 REDIS_CLI=`which redis-cli`
 PORT=6379
 
- 
+calc(){
+  awk "BEGIN { print "$*" }"
+}
+
+
 while sleep "$INTERVAL"
 do
 
+  # parsing stats
   #info=$(echo info|nc -w 1 127.0.0.1 $PORT)
   info=`$REDIS_CLI -p $PORT info`
   connected_clients=$(echo "$info"|awk -F : '$1 == "connected_clients" {print $2}')
@@ -22,17 +27,28 @@ do
   keyspace_hits=$(echo "$info"|awk -F : '$1 == "keyspace_hits" {print $2}')
   rejected_connections=$(echo "$info"|awk -F : '$1 == "rejected_connections" {print $2}')
   evicted_keys=$(echo "$info"|awk -F : '$1 == "evicted_keys" {print $2}')
-   
-  echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_connections-clients interval=$INTERVAL N:$connected_clients"
-  echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_connections-slaves interval=$INTERVAL N:$connected_slaves"
+
+  # calculation hit/miss ratio
+  keyspace_hit_miss_total=`calc $keyspace_misses+$keyspace_hits`
+  if [ $keyspace_hit_miss_total -gt 0 ]
+  then
+    keyspace_hit_miss_ratio=`calc $keyspace_hits/$keyspace_hit_miss_total`
+  else
+    keyspace_hit_miss_ratio=1
+  fi
+
+  # putting values into collectd
+  echo "PUTVAL $HOSTNAME/redis-$PORT/current_connections-clients interval=$INTERVAL N:$connected_clients"
+  echo "PUTVAL $HOSTNAME/redis-$PORT/current_connections-slaves interval=$INTERVAL N:$connected_slaves"
   echo "PUTVAL $HOSTNAME/redis-$PORT/uptime interval=$INTERVAL N:$uptime"
   echo "PUTVAL $HOSTNAME/redis-$PORT/df-memory interval=$INTERVAL N:$used_memory:U"
   echo "PUTVAL $HOSTNAME/redis-$PORT/files-unsaved_changes interval=$INTERVAL N:$changes_since_last_save"
-  echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_command-total interval=$INTERVAL N:$total_commands_processed"
-  echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_items-db0 interval=$INTERVAL N:$keys"
+  echo "PUTVAL $HOSTNAME/redis-$PORT/total_operations interval=$INTERVAL N:$total_commands_processed"
+  echo "PUTVAL $HOSTNAME/redis-$PORT/objects-keys-total interval=$INTERVAL N:$keys"
   echo "PUTVAL $HOSTNAME/redis-$PORT/objects-keyspace_misses interval=$INTERVAL N:$keyspace_misses"
   echo "PUTVAL $HOSTNAME/redis-$PORT/objects-keyspace_hits interval=$INTERVAL N:$keyspace_hits"
   echo "PUTVAL $HOSTNAME/redis-$PORT/connections-rejected interval=$INTERVAL N:$rejected_connections"
   echo "PUTVAL $HOSTNAME/redis-$PORT/objects-evicted_keys interval=$INTERVAL N:$evicted_keys"
+  echo "PUTVAL $HOSTNAME/redis-$PORT/cache_ratio-keyspace_hit_miss interval=$INTERVAL N:$keyspace_hit_miss_ratio"
 
 done
