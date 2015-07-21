8c925bb80bef9e5a2d92d7473fab2d58d87815b0
Honza Novak <kacerr.cz@gmail.com>
2015-07-21 16:34:50 +0200

    Add puppetdb plugin (java heap memory monitoring only for now)

---
+++ manifests/plugin/puppetdb.pp
@@ -0,0 +1,13 @@
+class collectd::plugin::puppetdb(
+  $listen_ip = hiera('collectd::plugin::puppetdb::listen_ip', $::ipaddress)
+)
+{
+  file { '/etc/collectd.d/puppetdb.conf':
+    content => template('collectd/puppetdb.conf.erb'),
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    require => Package['collectd'],
+    notify  => Service['collectd'],
+  }
+}
