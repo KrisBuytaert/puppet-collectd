ce76b164d04adafaeabdea81bd3810d917148cff
Pavel Pulec <kayn@inuits.eu>
2015-09-16 16:11:45 +0200

    do not edit haproxy.cfg otherwise haproxy is always restarted
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

--- manifests/plugin/haproxy.pp
+++ manifests/plugin/haproxy.pp
@@ -1,16 +1,18 @@
+# class collectd::plugin::haproxy
 class collectd::plugin::haproxy {
 
   file_line { 'haproxyline':
-  path => '/usr/share/collectd/types.db',
-  line => 'haproxy_backend   stot:COUNTER:0:U, econ:COUNTER:0:U, eresp:COUNTER:0:U',
-  match  => '^haproxy_backend\s+',
+    path  => '/usr/share/collectd/types.db',
+    line  => 'haproxy_backend   stot:COUNTER:0:U, econ:COUNTER:0:U, eresp:COUNTER:0:U',
+    match => '^haproxy_backend\s+',
   }
 
-  file_line { 'socket':
-  path => '/etc/haproxy/haproxy.cfg',
-  line => '  stats  socket /var/lib/haproxy/stats mode 600 level admin user haproxy group haproxy',
-  match  => '^  stats  socket',
-  }
+  # right permissions are configured in haproxy profile as default
+  # file_line { 'socket':
+  #   path => '/etc/haproxy/haproxy.cfg',
+  #   line => '  stats  socket /var/lib/haproxy/stats mode 600 level admin user haproxy group haproxy',
+  #   match  => '^  stats  socket',
+  # }
 
   package {'socat':
     ensure => 'present',
@@ -31,7 +33,7 @@ class collectd::plugin::haproxy {
     mode   => '0644',
     owner  => 'root',
     source => 'puppet:///modules/collectd/plugin/exec.conf',
-    notify  => Service['collectd'],
+    notify => Service['collectd'],
   }
 
 }
