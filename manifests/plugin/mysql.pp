943539ca203e6b9ba6dd1531437c72ff7c30029a
Pavel Pulec <kayn@inuits.eu>
2015-10-22 15:47:06 +0200

    add mysql plugin
    Signed-off-by: Pavel Pulec <kayn@inuits.eu>

---
+++ manifests/plugin/mysql.pp
@@ -0,0 +1,25 @@
+# class collectd::plugin::mysql
+class collectd::plugin::mysql(
+  $host = '127.0.0.1',
+  $user = 'root',
+  $password = '',
+){
+
+  File{
+    ensure => present,
+    group  => 'root',
+    mode   => '0644',
+    owner  => 'root',
+  }
+
+  file { '/usr/local/collectd-plugins/mysql.py':
+    source => 'puppet:///modules/collectd/plugin/mysql.py',
+  }
+
+  file { '/etc/collectd.d/mysql.conf':
+    content => template('collectd/mysql.conf.erb'),
+    require => File['/usr/local/collectd-plugins/mysql.py'],
+    notify  => Service['collectd'],
+  }
+
+}
