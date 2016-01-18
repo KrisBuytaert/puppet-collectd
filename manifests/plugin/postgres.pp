b6e86f6bee84c6734180e62986f232036d33f803
Kris Buytaert <sdog@inuits.eu>
2016-01-18 14:26:20 +0100

    Initial postgres support

---
+++ manifests/plugin/postgres.pp
@@ -0,0 +1,32 @@
+# Class: collectd
+#
+# This module manages collectd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+
+class collectd::plugin::postgres (
+  $dbname,
+  $dbuser,
+  $dbpass
+){
+
+  file { '/etc/collectd.d/postgres.conf':
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+    require => [ Package['collectd'],Package['collectd-postgresql'] ],
+    notify  => Service['collectd'],
+    content =>  template('collectd/postgres.conf.erb'),
+  }
+
+  package {'collectd-postgresql': ensure =>present }
+
+}
